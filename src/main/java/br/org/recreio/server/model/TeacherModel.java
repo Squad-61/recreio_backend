74b32e5e008da34ffdec8303f96cbf89c64f1cce
Guilherme Augusto F S <guilherme.frotas19@gmail.com>
2022-10-19 19:25:16 -0300

    [CHORE] Remove discrimination column

--- src/main/java/br/org/recreio/server/model/TeacherModel.java
+++ src/main/java/br/org/recreio/server/model/TeacherModel.java
@@ -1,14 +1,12 @@
 package br.org.recreio.server.model;
 
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.io.Serial;
 
 @Entity
 @Table(name = "Teacher")
-@DiscriminatorColumn(name = "Teacher")
 public class TeacherModel extends ProfileModel {
 
     @Serial
